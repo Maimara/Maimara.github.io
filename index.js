1a4cd1ffc1d13f2045ac226a343d82a488c78d4a
Maimara <121819003+Maimara@users.noreply.github.com>
2023-03-23 17:30:04 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+let botonExperienciauno = document.getElementById('botonExperienciauno');
+let parrafoExperienciauno = document.getElementById('parrafoExperienciauno');
+botonExperienciauno.addEventListener('click', alternarTexto);
+function alternarTexto() {
+    parrafoExperienciauno.classList.toggle('mostrarContenido');
+}
+
+let botonExperienciados = document.getElementById('botonExperienciados');
+let parrafoExperienciados = document.getElementById('parrafoExperienciados');
+botonExperienciados.addEventListener('click', alternarTextodos);
+function alternarTextodos() {
+    parrafoExperienciados.classList.toggle('mostrarContenido');
+}
+
+let botonExperienciatres = document.getElementById('botonExperienciatres');
+let parrafoExperienciatres = document.getElementById('parrafoExperienciatres');
+botonExperienciatres.addEventListener('click', alternarTextotres);
+function alternarTextotres() {
+    parrafoExperienciatres.classList.toggle('mostrarContenido');
+}
+
+let botonExperienciacuatro = document.getElementById('botonExperienciacuatro');
+let parrafoExperienciacuatro = document.getElementById('parrafoExperienciacuatro');
+botonExperienciacuatro.addEventListener('click', alternarTextocuatro);
+function alternarTextocuatro() {
+    parrafoExperienciacuatro.classList.toggle('mostrarContenido');
+}
+
+let botonExperienciacinco = document.getElementById('botonExperienciacinco');
+let parrafoExperienciacinco = document.getElementById('parrafoExperienciacinco');
+botonExperienciacinco.addEventListener('click', alternarTextocinco);
+function alternarTextocinco() {
+    parrafoExperienciacinco.classList.toggle('mostrarContenido');
+}
+
